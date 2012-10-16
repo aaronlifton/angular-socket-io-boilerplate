3d36695dd805342299df6e5f4f3b3878b68dcb2d
Aaron Lifton <theflubba@gmail.com>
2012-10-16 14:51:19 -0400

    working chat example

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-    $locationProvider.html5Mode(true);
-  }]);
+var app = angular.module('myApp', ['myApp.filters', 'myApp.directives'])
--- public/js/services.js
+++ public/js/services.js
@@ -5,7 +5,7 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+/*angular.module('myApp.services', []).
   value('version', '0.1').
   factory('socket', function ($rootScope) {
     var socket = io.connect();
@@ -30,3 +30,27 @@ angular.module('myApp.services', []).
       }
     };
   });
+*/
+app.factory('socket', function ($rootScope) {
+    var socket = io.connect();
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
